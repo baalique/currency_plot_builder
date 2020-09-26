29cc1df4bcd1e9238ee1206fca9d0a13febbfce2
bal410 <baalique@gmail.com>
2020-09-26 12:48:17 +0700

    Add currency API

---
+++ currency.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from datetime import date
+
+import requests
+
+
+@dataclass
+class CurrencyPair:
+    currency_x: str
+    currency_y: str
+
+
+class Currency:
+    def __init__(self, currencies: CurrencyPair):
+        self.currencies = currencies
+
+    def __str__(self):
+        return f'{self.currencies.currency_x} -> {self.currencies.currency_y}'
+
+    def get_rate(self, start_date: date, stop_date: date):
+        exchange_url = 'https://api.exchangeratesapi.io/'
+        url = f'{exchange_url}history?start_at={start_date}&end_at={stop_date}&' \
+              f'base={self.currencies.currency_x}&symbols={self.currencies.currency_y}'
+        response = requests.get(url)
+        return response.json()
+
+
+if __name__ == '__main__':
+    c = Currency(currencies=CurrencyPair('RUB', 'USD'))
+    d1 = date(year=2020, month=9, day=1)
+    d2 = date(year=2020, month=9, day=8)
+    print(c.get_rate(d1, d2))
